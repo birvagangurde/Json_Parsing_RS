d439fce40066368c1605ba4470d2e9188f7b1503
birvagangurde <gangurdebirva@gmail.com>
2025-09-30 01:53:59 +0530

    json schemas compare

---
+++ compare_jsons4.py
@@ -0,0 +1,27 @@
+# 27th Video
+
+
+import json
+
+# ****** Parse content present in Json file
+with open('C:\\Users\\BIRVA\\Downloads\\BackEndAutomation_Part1\\BackEndAutomation\\course.json') as f:
+    data = json.load(f)
+    print(data)
+    print(type(data))
+    print(data['courses'][1]['title'])
+    print(data['dashboard']['website'])
+    print(type(data['dashboard']))
+
+    print(type(data['courses']))
+    # The price of course "RPA"
+    for course in data['courses']:
+        print(course)
+        if course['title'] == "RPA":
+            print(course['price'])
+            assert course['price'] == 450
+
+# Actual code for this file: -----------------------------------------
+
+with open('C:\\Users\\BIRVA\\Downloads\\BackEndAutomation_Part1\\BackEndAutomation\\course1.json') as fi:
+    data2 = json.load(fi)
+    assert data == data2
